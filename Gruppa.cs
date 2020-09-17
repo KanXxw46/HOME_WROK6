dd8368c03b9a275820a86ef676167f0ef5095d48
Андрей Кан <1kan_andreikkkk@mail.ru>
2020-09-17 21:00:20 +0600

    Homework

---
+++ Gruppa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hw_06
+{
+    public class Gruppa
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data.Common;
+namespace Hw_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbProviderFactories.RegisterFactory("mssql", SqlClientFactory.Instance);
+            DbProviderFactory factory = DbProviderFactories.GetFactory("mssql");
+
+            DbConnection connection = factory.CreateConnection();
+            connection.ConnectionString = "Server = (localdb)\\MSSQLLocalDB;Database = Hw_06;Trusted_Connection = True";
+            connection.Open();
+
+            DbTransaction transaction = connection.BeginTransaction();
+
+            DbCommand command = factory.CreateCommand();
+            command.CommandText = $"insert into Users(Id,Name) values (@Id,@Name);";
+            command.Connection = connection;
+        }
+    }
+}
